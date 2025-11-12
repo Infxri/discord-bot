e5c9d9985a5e8dafd5d745eb32644405c2314738
inf <inferi2009@gmail.com>
2025-11-12 23:59:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+client.login(DISCORD_TOKEN);
+
+
 const DISCORD_TOKEN = process.env.DISCORD_TOKEN;
 const SPOTIFY_CLIENT_ID = process.env.SPOTIFY_CLIENT_ID;
 const SPOTIFY_CLIENT_SECRET = process.env.SPOTIFY_CLIENT_SECRET;
